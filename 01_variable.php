c0ca84c9d69b544808834fc598d23282419fc499
Jaleed khan <132694164+JaleedKhanofficial@users.noreply.github.com>
2025-06-14 12:16:40 +0500

    Add files via upload

---
+++ 01_variable.php
@@ -0,0 +1,14 @@
+<?php
+$name = "jaleed khan";
+$age = 24;
+$isFather = true;
+$money = 20;
+$salary = null;
+
+echo $name . "<br>";
+echo "Hello ". $name . "!<br>";
+echo "Hello {$name} <br>"; 
+print_r($name)."<br> <br>";
+var_dump($name);
+var_dump(is_string($age));
+?>
